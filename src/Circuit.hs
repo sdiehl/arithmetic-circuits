e871c0da520b650b05fcefddb0c39de2bd77bced
sdiehl <stephen.m.diehl@gmail.com>
2019-11-22 07:02:17 +0000

    Exports for Circuit.Expr

--- src/Circuit.hs
+++ src/Circuit.hs
@@ -1,8 +1,9 @@
-module Circuit (
-  module Circuit.Arithmetic,
-  module Circuit.Lang,
-  module Circuit.Expr,
-) where
+module Circuit
+  ( module Circuit.Arithmetic,
+    module Circuit.Lang,
+    module Circuit.Expr,
+  )
+where
 
 import Circuit.Arithmetic
 import Circuit.Expr
--- src/Circuit/Expr.hs
+++ src/Circuit/Expr.hs
@@ -1,13 +1,30 @@
 {-# LANGUAGE StandaloneDeriving #-}
 
-module Circuit.Expr where
-
-import Protolude
-
-import Text.PrettyPrint.Leijen.Text hiding ((<$>))
+module Circuit.Expr
+  ( UnOp (..),
+    BinOp (..),
+    Expr (..),
+    ExprM,
+    compile,
+    emit,
+    imm,
+    addVar,
+    addWire,
+    freshInput,
+    freshOutput,
+    runCircuitBuilder,
+    evalCircuitBuilder,
+    execCircuitBuilder,
+    exprToArithCircuit,
+    evalExpr,
+    truncRotate,
+  )
+where
 
 import Circuit.Affine
 import Circuit.Arithmetic
+import Protolude
+import Text.PrettyPrint.Leijen.Text hiding ((<$>))
 
 data UnOp f a where
   UNeg :: UnOp f f
@@ -24,7 +41,7 @@ data BinOp f a where
   BXor :: BinOp f Bool
 
 opPrecedence :: BinOp f a -> Int
-opPrecedence BOr  = 5
+opPrecedence BOr = 5
 opPrecedence BXor = 5
 opPrecedence BAnd = 5
 opPrecedence BSub = 6
@@ -38,14 +55,16 @@ data Expr i f ty where
   EConstBool :: Bool -> Expr i f Bool
   EVar :: i -> Expr i f f
   EVarBool :: i -> Expr i f Bool
-  EUnOp  :: UnOp f ty -> Expr i f ty -> Expr i f ty
-  EBinOp  :: BinOp f ty -> Expr i f ty -> Expr i f ty -> Expr i f ty
+  EUnOp :: UnOp f ty -> Expr i f ty -> Expr i f ty
+  EBinOp :: BinOp f ty -> Expr i f ty -> Expr i f ty -> Expr i f ty
   EIf :: Expr i f Bool -> Expr i f ty -> Expr i f ty -> Expr i f ty
   EEq :: Expr i f f -> Expr i f f -> Expr i f Bool
 
-deriving instance (Show i, Show f) => Show (Expr i f ty )
-deriving instance ( Show f) => Show (BinOp f a )
-deriving instance (Show f) => Show (UnOp f a )
+deriving instance (Show i, Show f) => Show (Expr i f ty)
+
+deriving instance (Show f) => Show (BinOp f a)
+
+deriving instance (Show f) => Show (UnOp f a)
 
 instance Pretty (BinOp f a) where
   pretty op = case op of
@@ -60,34 +79,34 @@ instance Pretty (UnOp f a) where
   pretty op = case op of
     UNeg -> text "neg"
     UNot -> text "!"
-    URot truncBits rotBits  -> text "rot(" <> pretty truncBits <> text "," <> pretty rotBits <> ")"
+    URot truncBits rotBits -> text "rot(" <> pretty truncBits <> text "," <> pretty rotBits <> ")"
 
 instance (Pretty f, Pretty i, Pretty ty) => Pretty (Expr i f ty) where
   pretty = prettyPrec 0
     where
       prettyPrec :: (Pretty f, Pretty i, Pretty ty) => Int -> Expr i f ty -> Doc
-      prettyPrec p e
-        = case e of
-            EVar v
-              -> pretty v
-            EVarBool v
-              -> pretty v
-            EConst l
-              -> pretty l
-            EConstBool b
-              -> pretty b
-            -- TODO correct precedence
-            EUnOp op e1 -> parens (pretty op <+> pretty e1)
-            EBinOp op e1 e2
-              -> parensPrec (opPrecedence op) p
-                   $ prettyPrec (opPrecedence op) e1
-                   <+> pretty op
-                   <+> prettyPrec (opPrecedence op) e2
-            EIf b true false
-              -> parensPrec 4 p (text "if" <+> pretty b <+> text "then" <+> pretty true <+> text "else" <+> pretty false)
-            -- TODO correct precedence
-            EEq l r
-              -> parensPrec 1 p (pretty l) <+> text "=" <+> parensPrec 1 p (pretty r)
+      prettyPrec p e =
+        case e of
+          EVar v ->
+            pretty v
+          EVarBool v ->
+            pretty v
+          EConst l ->
+            pretty l
+          EConstBool b ->
+            pretty b
+          -- TODO correct precedence
+          EUnOp op e1 -> parens (pretty op <+> pretty e1)
+          EBinOp op e1 e2 ->
+            parensPrec (opPrecedence op) p $
+              prettyPrec (opPrecedence op) e1
+                <+> pretty op
+                <+> prettyPrec (opPrecedence op) e2
+          EIf b true false ->
+            parensPrec 4 p (text "if" <+> pretty b <+> text "then" <+> pretty true <+> text "else" <+> pretty false)
+          -- TODO correct precedence
+          EEq l r ->
+            parensPrec 1 p (pretty l) <+> text "=" <+> parensPrec 1 p (pretty r)
 
 parensPrec :: Int -> Int -> Doc -> Doc
 parensPrec opPrec p = if p > opPrec then parens else identity
@@ -98,55 +117,64 @@ parensPrec opPrec p = if p > opPrec then parens else identity
 
 -- | Truncate a number to the given number of bits and perform a right
 -- rotation (assuming small-endianness) within the truncation.
-truncRotate
-  :: (Bits f)
-  => Int -- ^ number of bits to truncate to
-  -> Int -- ^ number of bits to rotate by
-  -> f
-  -> f
+truncRotate ::
+  (Bits f) =>
+  -- | number of bits to truncate to
+  Int ->
+  -- | number of bits to rotate by
+  Int ->
+  f ->
+  f
 truncRotate nbits nrots x =
-  foldr (\ix rest -> if testBit x ix
-                     then setBit rest ((ix + nrots) `mod` nbits)
-                     else rest
-        )
-        zeroBits
-        [0..nbits - 1]
+  foldr
+    ( \ix rest ->
+        if testBit x ix
+          then setBit rest ((ix + nrots) `mod` nbits)
+          else rest
+    )
+    zeroBits
+    [0 .. nbits - 1]
 
 -- | Evaluate arithmetic expressions directly, given an environment
-evalExpr
-  :: (Bits f, Num f)
-  => (i -> vars -> Maybe f) -- ^ variable lookup
-  -> Expr i f ty -- ^ expression to evaluate
-  -> vars -- ^ input values
-  -> ty -- ^ resulting value
+evalExpr ::
+  (Bits f, Num f) =>
+  -- | variable lookup
+  (i -> vars -> Maybe f) ->
+  -- | expression to evaluate
+  Expr i f ty ->
+  -- | input values
+  vars ->
+  -- | resulting value
+  ty
 evalExpr lookupVar expr vars = case expr of
-  EConst     f -> f
+  EConst f -> f
   EConstBool b -> b
-  EVar       i -> case lookupVar i vars of
-    Just v  -> v
+  EVar i -> case lookupVar i vars of
+    Just v -> v
     Nothing -> panic "TODO: incorrect var lookup"
   EVarBool i -> case lookupVar i vars of
-    Just v  -> v == 1
+    Just v -> v == 1
     Nothing -> panic "TODO: incorrect var lookup"
-  EUnOp UNeg e1
-    -> negate $ evalExpr lookupVar e1 vars
-  EUnOp UNot e1
-    -> not $ evalExpr lookupVar e1 vars
-  EUnOp (URot truncBits rotBits) e1
-    -> truncRotate truncBits rotBits $ evalExpr lookupVar e1 vars
+  EUnOp UNeg e1 ->
+    negate $ evalExpr lookupVar e1 vars
+  EUnOp UNot e1 ->
+    not $ evalExpr lookupVar e1 vars
+  EUnOp (URot truncBits rotBits) e1 ->
+    truncRotate truncBits rotBits $ evalExpr lookupVar e1 vars
   EBinOp op e1 e2 ->
     (evalExpr lookupVar e1 vars) `apply` (evalExpr lookupVar e2 vars)
-   where
-    apply = case op of
-      BAdd -> (+)
-      BSub -> (-)
-      BMul -> (*)
-      BAnd -> (&&)
-      BOr  -> (||)
-      BXor -> \x y -> (x || y) && not (x && y)
-  EIf b true false -> if evalExpr lookupVar b vars
-    then evalExpr lookupVar true vars
-    else evalExpr lookupVar false vars
+    where
+      apply = case op of
+        BAdd -> (+)
+        BSub -> (-)
+        BMul -> (*)
+        BAnd -> (&&)
+        BOr -> (||)
+        BXor -> \x y -> (x || y) && not (x && y)
+  EIf b true false ->
+    if evalExpr lookupVar b vars
+      then evalExpr lookupVar true vars
+      else evalExpr lookupVar false vars
   EEq lhs rhs -> evalExpr lookupVar lhs vars == evalExpr lookupVar rhs vars
 
 -------------------------------------------------------------------------------
@@ -157,14 +185,16 @@ type ExprM f a = State (ArithCircuit f, Int) a
 
 execCircuitBuilder :: ExprM f a -> ArithCircuit f
 execCircuitBuilder m = reverseCircuit $ fst $ execState m (ArithCircuit [], 0)
-  where reverseCircuit = (\(ArithCircuit cs) -> ArithCircuit $ reverse cs)
+  where
+    reverseCircuit = (\(ArithCircuit cs) -> ArithCircuit $ reverse cs)
 
 evalCircuitBuilder :: ExprM f a -> a
 evalCircuitBuilder = fst . runCircuitBuilder
 
 runCircuitBuilder :: ExprM f a -> (a, ArithCircuit f)
 runCircuitBuilder m = second (reverseCircuit . fst) $ runState m (ArithCircuit [], 0)
-  where reverseCircuit = (\(ArithCircuit cs) -> ArithCircuit $ reverse cs)
+  where
+    reverseCircuit = (\(ArithCircuit cs) -> ArithCircuit $ reverse cs)
 
 fresh :: ExprM f Int
 fresh = do
@@ -207,11 +237,11 @@ addWire (Right c) = do
 
 compile :: Num f => Expr Wire f ty -> ExprM f (Either Wire (AffineCircuit Wire f))
 compile expr = case expr of
-  EConst     n -> pure . Right $ ConstGate n
+  EConst n -> pure . Right $ ConstGate n
   EConstBool b -> pure . Right $ ConstGate (if b then 1 else 0)
-  EVar       v -> pure . Left $ v
-  EVarBool   v -> pure . Left $ v
-  EUnOp op e1  -> do
+  EVar v -> pure . Left $ v
+  EVarBool v -> pure . Left $ v
+  EUnOp op e1 -> do
     e1Out <- compile e1
     case op of
       UNeg -> pure . Right $ ScalarMul (-1) (addVar e1Out)
@@ -246,19 +276,16 @@ compile expr = case expr of
         tmp1 <- imm
         emit $ Mul e1Out e2Out tmp1
         pure . Right $ Add (Add e1Out e2Out) (ScalarMul (-2) (Var tmp1))
-
   -- IF(cond, true, false) = (cond*true) + ((!cond) * false)
   EIf cond true false -> do
-    condOut  <- addVar <$> compile cond
-    trueOut  <- addVar <$> compile true
+    condOut <- addVar <$> compile cond
+    trueOut <- addVar <$> compile true
     falseOut <- addVar <$> compile false
-
-    tmp1     <- imm
-    tmp2     <- imm
+    tmp1 <- imm
+    tmp2 <- imm
     emit $ Mul condOut trueOut tmp1
     emit $ Mul (Add (ConstGate 1) (ScalarMul (-1) condOut)) falseOut tmp2
     pure . Right $ Add (Var tmp1) (Var tmp2)
-
   -- EQ(lhs, rhs) = (lhs - rhs == 1)
   EEq lhs rhs -> do
     lhsSubRhs <- compile (EBinOp BSub lhs rhs)
@@ -271,11 +298,13 @@ compile expr = case expr of
     pure . Right $ Add (ConstGate 1) (ScalarMul (-1) (Var eqOutWire))
 
 -- | Translate an arithmetic expression to an arithmetic circuit
-exprToArithCircuit
-  :: Num f
-  => Expr Int f ty -- ^ expression to compile
-  -> Wire  -- ^ Wire to assign the output of the expression to
-  -> ExprM f ()
+exprToArithCircuit ::
+  Num f =>
+  -- | expression to compile
+  Expr Int f ty ->
+  -- | Wire to assign the output of the expression to
+  Wire ->
+  ExprM f ()
 exprToArithCircuit expr output =
   exprToArithCircuit' (mapVarsExpr InputWire expr) output
 
@@ -287,11 +316,11 @@ exprToArithCircuit' expr output = do
 -- | Apply function to variable names.
 mapVarsExpr :: (i -> j) -> Expr i f ty -> Expr j f ty
 mapVarsExpr f expr = case expr of
-  EVar       i    -> EVar $ f i
-  EVarBool   i    -> EVarBool $ f i
-  EConst     v    -> EConst v
-  EConstBool b    -> EConstBool b
+  EVar i -> EVar $ f i
+  EVarBool i -> EVarBool $ f i
+  EConst v -> EConst v
+  EConstBool b -> EConstBool b
   EBinOp op e1 e2 -> EBinOp op (mapVarsExpr f e1) (mapVarsExpr f e2)
-  EUnOp op e1     -> EUnOp op (mapVarsExpr f e1)
-  EIf b tr fl     -> EIf (mapVarsExpr f b) (mapVarsExpr f tr) (mapVarsExpr f fl)
-  EEq lhs rhs     -> EEq (mapVarsExpr f lhs) (mapVarsExpr f rhs)
+  EUnOp op e1 -> EUnOp op (mapVarsExpr f e1)
+  EIf b tr fl -> EIf (mapVarsExpr f b) (mapVarsExpr f tr) (mapVarsExpr f fl)
+  EEq lhs rhs -> EEq (mapVarsExpr f lhs) (mapVarsExpr f rhs)
