a9ed1c40504f997b5036098ee58bafcd4b62dfbd
sdiehl <stephen.m.diehl@gmail.com>
2020-01-09 08:10:13 +0000

    Setup Github Actions

--- default.nix
+++ default.nix
@@ -1,5 +1,35 @@
-{ pkgs ? import <nixpkgs> {}
+{ mkDerivation, aeson, base, bulletproofs, containers, criterion
+, elliptic-curve, filepath, galois-fft, galois-field, hpack
+, markdown-unlit, MonadRandom, pairing, poly, process-extras
+, protolude, QuickCheck, quickcheck-instances, semirings, stdenv
+, tasty, tasty-discover, tasty-hunit, tasty-quickcheck, text
+, vector, wl-pprint-text
 }:
-pkgs.haskellPackages.developPackage {
-  root = ./.;
+mkDerivation {
+  pname = "arithmetic-circuits";
+  version = "0.2.0";
+  src = ./arithmetic-circuits;
+  libraryHaskellDepends = [
+    aeson base bulletproofs containers elliptic-curve filepath
+    galois-fft galois-field MonadRandom poly process-extras protolude
+    semirings text vector wl-pprint-text
+  ];
+  libraryToolDepends = [ hpack ];
+  testHaskellDepends = [
+    aeson base bulletproofs containers elliptic-curve filepath
+    galois-fft galois-field markdown-unlit MonadRandom pairing poly
+    process-extras protolude QuickCheck quickcheck-instances semirings
+    tasty tasty-discover tasty-hunit tasty-quickcheck text vector
+    wl-pprint-text
+  ];
+  testToolDepends = [ markdown-unlit tasty-discover ];
+  benchmarkHaskellDepends = [
+    aeson base bulletproofs containers criterion elliptic-curve
+    filepath galois-fft galois-field MonadRandom pairing poly
+    process-extras protolude semirings text vector wl-pprint-text
+  ];
+  prePatch = "hpack";
+  homepage = "https://github.com/adjoint-io/arithmetic-circuits#readme";
+  description = "Arithmetic circuits for zkSNARKs";
+  license = stdenv.lib.licenses.mit;
 }
